4e33f6a0ccbaaf2402c7f234faef0ea2255b7856
Natalie Mertens <natmertens@gmx.de>
2021-04-12 19:56:11 +0200

    added info and error alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
   state = {
     query: '',
     suggestions: [],
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: ''
   }
 
   handleInputChanged = (event) => {
@@ -13,10 +15,18 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We cannot find the city you are looking for. Please try another city.'
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   }
 
   handleItemClicked = (suggestion) => {
@@ -31,6 +41,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
   state = {
-    numberOfEvents: this.props.numberOfEvents
+    /*numberOfEvents: this.props.numberOfEvents,*/
+    errorText: ''
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+
     this.setState({
-      numberOfEvents: value
+      /*numberOfEvents: value,*/
+      errorText: ''
     });
+
+    if (value < 1 || value > 32) {
+      this.setState({
+        errorText: 'Please select a number between 1 and 32'
+      });
+    }
 
     const location = this.props.selectedLocation ?
       this.props.selectedLocation :
       'all';
-
     this.props.updateEvents(location, value);
   }
 
@@ -30,6 +39,7 @@ class NumberOfEvents extends Component {
           placeholder="12"
           onChange={this.handleInputChanged}
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
