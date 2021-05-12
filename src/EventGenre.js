53ae6832861ea6446b943155a0a871478e663117
Natalie Mertens <natmertens@gmx.de>
2021-05-12 17:37:48 +0200

    cleaned up code

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  PieChart, Pie, Cell, ResponsiveContainer
+  PieChart, Pie, ResponsiveContainer
 } from 'recharts';
 
 const EventGenre = ({ events }) => {
