1d796939f14fdced29c5cc6902ac1bea55f18d08
Natalie Mertens <natmertens@gmx.de>
2021-07-24 10:50:52 +0200

    updated EventGenre

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -31,7 +31,7 @@ const EventGenre = ({ events }) => {
     <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
         <Pie
-          data={getData()}
+          data={data}
           cx={200}
           cy={200}
           labelLine={false}
